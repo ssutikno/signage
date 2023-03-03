bc52564d940e5fd39157757675790e13886f84ab
Sugiyanto Sutikno <sugiyanto.sutikno@gmail.com>
2023-03-03 17:51:04 +0700

    app update

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -5,12 +6,28 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const videoDir = './public/videos/';
 
 var app = express();
+
+const videoFiles = fs.readdirSync(videoDir).filter(file => {
+    return file.endsWith('.mp4') || file.endsWith('.avi') || file.endsWith('.mov');
+})
+
+// sort the video files aphabetically
+videoFiles.sort();
+
+// Create the playlist from the sorted video files
+const playlist = videoFile.map(file =>{
+    return file;
+})
+
 
 app.use(logger('dev'));
 // Use the EJS template engine
 app.set('view engine', 'ejs');
+app.use('/videos', express.static(path.join(__dirname, 'public/videos')))
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
